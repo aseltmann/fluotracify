2e3d7e0ca2b18ae6ac132923678014f0202d1b58
Apoplex <oligolex@vivaldi.net>
2020-04-28 16:36:59 +0200

    isort workaround and sys.path.append as sys.argv

--- src/fluotracify/training/train.py
+++ src/fluotracify/training/train.py
@@ -4,9 +4,12 @@ import mlflow
 import mlflow.tensorflow
 import tensorflow as tf
 
-sys.path.append('/home/lex/Programme/drmed-git/src/')
-from fluotracify.simulations import import_simulation_from_csv as isfc
-from fluotracify.training import build_model as bm, preprocess_data as ppd
+fluotracify_path = float(sys.argv[1]) if len(sys.argv) > 1 else '~/Programme/drmed-git/src/'
+sys.path.append(fluotracify_path)
+
+if True:  # isort workaround
+    from fluotracify.simulations import import_simulation_from_csv as isfc
+    from fluotracify.training import build_model as bm, preprocess_data as ppd
 
 print(tf.__version__)
 
@@ -38,13 +41,13 @@ if __name__ == "__main__":
     # Cleanup
     del train, test
 
-    batch_size = float(sys.argv[1]) if len(sys.argv) > 1 else 5
+    batch_size = float(sys.argv[2]) if len(sys.argv) > 2 else 5
     batch_size = int(batch_size)
-    frac_val = float(sys.argv[2]) if len(sys.argv) > 2 else 0.2
-    length_delimiter = float(sys.argv[3]) if len(sys.argv) > 3 else 16384
+    frac_val = float(sys.argv[3]) if len(sys.argv) > 3 else 0.2
+    length_delimiter = float(sys.argv[4]) if len(sys.argv) > 4 else 16384
     length_delimiter = int(length_delimiter)
-    learning_rate = float(sys.argv[4]) if len(sys.argv) > 4 else 1e-5
-    epochs = float(sys.argv[5]) if len(sys.argv) > 5 else 10
+    learning_rate = float(sys.argv[5]) if len(sys.argv) > 5 else 1e-5
+    epochs = float(sys.argv[6]) if len(sys.argv) > 6 else 10
     epochs = int(epochs)
 
     dataset_train, dataset_val, num_train_examples, num_val_examples = ppd.tfds_from_pddf_for_unet(
