72b99efc123d0fec59a8199bd30fde3482f73100
Apoplex <oligolex@vivaldi.net>
2020-05-15 02:41:43 +0200

    Mlflow fix autolog (now with model and metrics)

--- src/fluotracify/training/train.py
+++ src/fluotracify/training/train.py
@@ -11,11 +11,10 @@ if True:  # isort workaround
     from fluotracify.simulations import import_simulation_from_csv as isfc
     from fluotracify.training import build_model as bm, preprocess_data as ppd
 
-print(tf.__version__)
-print('fluotracify path: ', fluotracify_path)
+print(tf.version.VERSION)
 print('GPUs: ', tf.config.list_physical_devices('GPU'))
 
-mlflow.tensorflow.autolog()
+mlflow.tensorflow.autolog(every_n_iter=1)
 
 if __name__ == "__main__":
     _batch_size = int(sys.argv[2]) if len(sys.argv) > 2 else 5
@@ -63,34 +62,26 @@ if __name__ == "__main__":
         batch_size=_batch_size,
         length_delimiter=length_delimiter)
 
-    with mlflow.start_run():
-        model = bm.unet_1d_alt(input_size=length_delimiter)
-        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-        loss = bm.binary_ce_dice_loss
+    model = bm.unet_1d_alt(input_size=length_delimiter)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    loss = bm.binary_ce_dice_loss()
 
-        model.compile(loss=loss,
-                      optimizer=optimizer,
-                      metrics=[
-                          tf.keras.metrics.MeanIoU(num_classes=2),
-                          tf.keras.metrics.Precision(),
-                          tf.keras.metrics.Recall()
-                      ])
+    model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=[
+                      tf.keras.metrics.Precision(),
+                      tf.keras.metrics.Recall()
+                  ])
 
-        history = model.fit(
-            x=dataset_train,
-            epochs=epochs,
-            steps_per_epoch=tf.math.ceil(num_train_examples / _batch_size),
-            validation_data=dataset_val,
-            validation_steps=tf.math.ceil(num_val_examples / _batch_size))
+    model.fit(x=dataset_train,
+              epochs=epochs,
+              steps_per_epoch=int(
+                  tf.math.ceil(num_train_examples / _batch_size).numpy()),
+              validation_data=dataset_val,
+              validation_steps=int(
+                  tf.math.ceil(num_val_examples / _batch_size).numpy()))
 
-        for i in history.epoch:
-            metrics = {}
-            for metric_name in history.history:
-                metric_value = history.history[metric_name][i]
-                metrics[metric_name] = metric_value
-            mlflow.log_metrics(metrics=metrics, step=i)
+    model.evaluate(dataset_test,
+                   steps=tf.math.ceil(num_test_examples / _batch_size))
 
-        model.evaluate(dataset_test,
-                       steps=tf.math.ceil(num_test_examples / _batch_size))
-
-        model.save('data/exp-devtest/unet.tf', save_format='tf')
+    model.save('data/exp-devtest/unet.tf', save_format='tf')
