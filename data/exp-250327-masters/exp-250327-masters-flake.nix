002c9e61b8c041f14c41eabc02098526ac8bfbf3
apolea <apolea@vivaldi.net>
2025-10-14 12:03:23 +0200

    add flake.nix

---
+++ data/exp-250327-masters/exp-250327-masters-flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Flake for exp-250327-masters branch of drmed-git repository";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {nixpkgs, flake-utils, ...}:
+
+flake-utils.lib.eachDefaultSystem (system:
+let
+  pkgs = import nixpkgs { inherit system; };
+in
+  {
+    devShells.default = pkgs.mkShell {
+      packages =
+        with pkgs; [
+          pdf2svg
+          python312
+        ] ++ (
+          with pkgs.python313Packages; [
+            click
+            cython
+            ipykernel
+            ipywidgetws
+            jupyterlab
+            lmfit
+            matplotlib
+            mlcroissant
+            mlflow
+            numpy
+            pandas
+            polars
+            scikit-image
+            scikit-learn
+            scipy
+            seaborn
+            tensorflow
+            tqdm
+          ]);
+      shellHook = ''
+        jupyter lab
+      '';
+    };
+  });
+}
