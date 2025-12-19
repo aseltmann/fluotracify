21711427228df37116c725862ffe116b5f1916b4
alva <apolea@vivaldi.net>
2025-12-19 18:04:21 +0100

    remove emacs & again from flake

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -64,7 +64,6 @@
                   polars  # 1.31.0  # polars jumped from 1.27.1 to 1.31.0 in nixpkgs, choose higher version
                 ]);
           shellHook = ''
-            emacs &
             jupyter lab
           '';
         };
