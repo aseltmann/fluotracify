bb488a4490981040d951615c28216434e8aaa2aa
alva <apolea@vivaldi.net>
2025-12-29 14:50:06 +0100

    remove jupyter lab shell hook; remove direnv allow

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -63,9 +63,9 @@
                 with pkgs-unstable.python312Packages; [
                   polars  # 1.31.0  # polars jumped from 1.27.1 to 1.31.0 in nixpkgs, choose higher version
                 ]);
-          shellHook = ''
-            jupyter lab
-          '';
+          # shellHook = ''
+
+          # '';
         };
       });
 }
