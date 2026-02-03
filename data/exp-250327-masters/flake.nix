6598d8fefdc4370a67ef3a7f2ad54ae4ac9eaad8
apolea <apolea@vivaldi.net>
2026-02-03 12:43:06 +0100

    add more cuda unfree

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -22,7 +22,7 @@
              "cuda_nvdisasm" "cuda_nvprune" "cuda_cudart" "cuda_cupti"
              "cuda_cuxxfilt" "cuda_nvml_dev" "cuda_nvrtc" "cuda_profiler_api"
              "cuda_nvtx" "cuda_sanitizer_api" "libcublas" "libcufft" "libcurand"
-             "libcusolver" "libnvjitlink"
+             "libcusolver" "libnvjitlink" "libcusparse"
            ];
          };
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
