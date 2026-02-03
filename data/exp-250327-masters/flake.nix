93128ec2312498230cb25a651e092906ec078162
apolea <apolea@vivaldi.net>
2026-02-03 12:35:53 +0100

    add more cuda unfree

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -18,7 +18,7 @@
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-             "cuda-merged" "cuda_cuobjdump"
+             "cuda-merged" "cuda_cuobjdump" "cuda_gdb"
            ];
          };
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
