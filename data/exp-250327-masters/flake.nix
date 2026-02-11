97f982a506975a832989079f7522593a11418681
apolea <apolea@vivaldi.net>
2026-02-11 18:08:45 +0100

    try tf-keras for keras2 in tf 2.19

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -1,9 +1,10 @@
+
 {
   description = "Flake for exp-250327-masters branch of drmed-git repository";
 
   inputs = {
-    # version 2024-05-19 for tf15 and keras2
-    nixpkgs.url = "github:nixos/nixpkgs?ref=bcdbb17a41d8fab94e8031a7c5e180fa5acd0809";
+    # version of nixos-25.05 from 2025-12-18
+    nixpkgs.url = "github:nixos/nixpkgs?ref=2b0d2b456e4e8452cf1c16d00118d145f31160f9";
     # version of nixos-unstable from 2025-07-06
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=55b0d38442aac04f892f03b6a53cd9bb4c6cfc1c";
     flake-utils.url = "github:numtide/flake-utils";
@@ -17,7 +18,7 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
         pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-        multipletau-pypi = pkgs.python311Packages.buildPythonPackage rec {
+        multipletau-pypi = pkgs.python312Packages.buildPythonPackage rec {
           pname = "multipletau";
           version = "0.4.1";
           pyproject = true;
@@ -25,11 +26,11 @@
             inherit pname version;
             hash = "sha256-roP342FbjWKEtx32KSe6Ibgy0eiwLHgqoFwXUlnuVO8=";
           };
-          build-system = with pkgs.python311Packages; [
+          build-system = with pkgs.python312Packages; [
             setuptools
             setuptools-scm
           ];
-          dependencies = with pkgs.python311Packages; [
+          dependencies = with pkgs.python312Packages; [
             numpy
           ];
         };
@@ -38,16 +39,18 @@
           packages =
             with pkgs; [
               pdf2svg
-              python311
+              python312  # 3.12.12
             ] ++ (
-              with pkgs.python311Packages; [
+              with pkgs.python312Packages; [
                 click  # 8.1.8
                 cython  # 3.0.12
                 ipykernel  # 6.29.5
                 ipywidgets  # 8.1.5 (conda env: 8.1.7)
                 jupyterlab  # 4.4.1
+                tf-keras
                 lmfit  # 1.3.3
                 matplotlib  # 3.10.1
+                mlcroissant  # 1.0.17
                 mlflow  # 2.20.3 (conda 2.21.3)
                 multipletau-pypi  # 0.4.1
                 numpy  # 2.2.5
@@ -55,14 +58,13 @@
                 scikit-image  # 0.25.2
                 scikit-learn  # 1.6.1
                 scipy  # 1.15.3 (conda 1.15.2)
-                seaborn  # 0.13.2 
-                tensorflow-bin  # 2.19.0
+                seaborn  # 0.13.2
+                tensorflow  # 2.19.0
                 tqdm  # 4.67.1
-              ]);
-          # ++ (
-          #       with pkgs-unstable.python312Packages; [
-          #         polars  # 1.31.0  # polars jumped from 1.27.1 to 1.31.0 in nixpkgs, choose higher version
-          #       ]);
+              ]) ++ (
+                with pkgs-unstable.python312Packages; [
+                  polars  # 1.31.0  # polars jumped from 1.27.1 to 1.31.0 in nixpkgs, choose higher version
+                ]);
           # shellHook = ''
 
           # '';
