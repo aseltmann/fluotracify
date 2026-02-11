38e2c64140e605172497ca1ea759d1f7688d2c0f
apolea <apolea@vivaldi.net>
2026-02-11 17:57:48 +0100

    try tensorflow-bin

--- data/exp-250327-masters/flake.nix
+++ data/exp-250327-masters/flake.nix
@@ -56,7 +56,7 @@
                 scikit-learn  # 1.6.1
                 scipy  # 1.15.3 (conda 1.15.2)
                 seaborn  # 0.13.2 
-                tensorflow  # 2.19.0
+                tensorflow-bin  # 2.19.0
                 tqdm  # 4.67.1
               ]);
           # ++ (
